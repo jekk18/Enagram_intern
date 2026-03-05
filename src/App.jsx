d277813484388b5835f7ae8f481c82f433453a20
Jeko <chelidzejeko9123741jeko@gmail.com>
2026-03-05 17:33:38 +0400

    update header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useState } from 'react'  
 import WebRoutes from './router/WebRoutes';
-import Header from './components/layouts/header';
+import Header from './components/layouts/Header';
 import MainContent from './components/Main/MainContent';
 
 
